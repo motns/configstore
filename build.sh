f71916b9ee8d86097aaeb5f97d322393f186715a
Adam Borocz <boroczadam@gmail.com>
2017-10-16 19:06:37 +0100

    Update build script to work with new layout

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ mkdir -p bin/linux
 mkdir -p bin/darwin
 mkdir -p bin/windows
 
-GOOS=darwin GOARCH=amd64 go build -o bin/darwin/configstore
-GOOS=linux GOARCH=amd64 go build -o bin/linux/configstore
-GOOS=windows GOARCH=amd64 go build -o bin/windows/configstore
+GOOS=darwin GOARCH=amd64 go build -v -o bin/darwin/configstore cmd/configstore/**
+GOOS=linux GOARCH=amd64 go build -v -o bin/linux/configstore cmd/configstore/**
+GOOS=windows GOARCH=amd64 go build -v -o bin/windows/configstore cmd/configstore/**
