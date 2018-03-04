74e046c508b8c678d02981fcda5c7d13f347658b
Adam Borocz <boroczadam@gmail.com>
2018-03-04 18:44:45 +0000

    Add comments in build script

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@ mkdir -p bin/linux
 mkdir -p bin/darwin
 mkdir -p bin/windows
 
+echo "=== Building 64bit MacOS binary..."
 GOOS=darwin GOARCH=amd64 go build -v -o bin/darwin/configstore cmd/configstore/**
+echo ""
+
+echo "=== Building 64bit Linux binary..."
 GOOS=linux GOARCH=amd64 go build -v -o bin/linux/configstore cmd/configstore/**
+echo ""
+
+echo "=== Building 64bit Windows binary..."
 GOOS=windows GOARCH=amd64 go build -v -o bin/windows/configstore cmd/configstore/**
+echo ""
